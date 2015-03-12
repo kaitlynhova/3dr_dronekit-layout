f3191715e6f27354d9da3ea069c365b6fd5c22b7
kaitlynhova <kaitlynhova@gmail.com>
2015-03-12 15:47:56 -0700

    scroll spy subnav attempt #1

--- js/scrollspy.js
+++ js/scrollspy.js
@@ -1,3 +1,7 @@
 $( document ).ready(function() {
   $('body').scrollspy({ target: '#my-nav' });
+
+  $('#moose').on('activate.bs.scrollspy', function() {
+      $('#subnav1').addClass('show-subnav');
+  });
 });
